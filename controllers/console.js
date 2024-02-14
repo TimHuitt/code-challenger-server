122cc9bb8032d8d939190110245b21a56185db68
Tim Huitt <timhuitt@gmail.com>
2024-02-13 17:33:06 -0700

    update console prompt

--- controllers/console.js
+++ controllers/console.js
@@ -19,7 +19,7 @@ async function consoleText(req, res) {
 - Code evaluation for 'true' requires exact match with the challenge's expected console output. If mismatched, detail the reasons within 'output' and return 'eval' as false.
 - Your feedback will be integrated into a 'console' simulation app, so format your responses accordingly.
 - Interpret the provided code string as including line breaks and indentation true to actual code formatting.
-- Never require Code execution or demonstration be included in user code. Do not address a lack of console output.
+- Never require Code execution or demonstration be included in user code. Never comment on a lack of console output or that the provided code does not produce console output.
 - To assess if the code is valid, you should formulate several test cases based on the provided challenge requirements. If you find any syntax errors or any test cases fail, you should return 'eval' as false.
 - If you output any errors, failures to meet the challenge requirements, failures to evaluate, or language mismatch, 'eval' must return as false
 - Process:
